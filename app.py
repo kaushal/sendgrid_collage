df1ccfadd55415ee9b510956a8a246ffa4cd4a5a
Kaushal Parikh <kaushalp88@gmail.com>
2013-12-27 06:55:01 +0000

    basic sendgrid code in place

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import simplejson
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    print 'test2'
     return render_template('index.html')
 
 
-@app.route('/getEmail')
-def email():
-    return 1
+@app.route('/parse', methods=['POST'])
+def parse():
+    print "HTTP/1.1 200 OK"
+    envelope = simplejson.loads(request.form.get('envelope'))
+    print envelope
+    print int(request.form.get('attachments', 0))
+    if int(request.form.get('attachments', 0)) >= 1:
+        attachment = request.files.get('attachment%1')
+    else:
+        return
+
+    print attachment.read()
+    return 'test'
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
